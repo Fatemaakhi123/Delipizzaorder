f9252b0409e206fe414409175c28bfe3874fdd2c
Fatemaakhi123 <fatematuzzohraakhi11@gmail.com>
2022-03-15 11:15:36 +0600

    project2

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function()
+{
+    $(".hide").click(function(){
+        $(".bg").toggle();
+        
+      });
+      $(".item").hide();
+      $(".menu-card").click(function(){
+        $(".item").toggle();
+        
+      });
+      $(".item1").hide();
+      $(".menu-card1").click(function(){
+        $(".item1").toggle();
+        
+      });
+      $(".item2").hide();
+      $(".menu-card2").click(function(){
+        $(".item2").toggle();
+        
+      });
+
+});
